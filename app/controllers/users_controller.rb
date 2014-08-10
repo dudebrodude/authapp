154f1f3c254cc1cd3d268e671ccf7fbe33d07aab
Daniel Babicky <dudebrodudelol@gmail.com>
2014-08-09 20:34:47 -0700

    added user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -7,13 +13,23 @@ class UsersController < ApplicationController
   def new
   	    @user = User.new
   end
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to Streemfeed!"
+  def edit
+  end
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
+      render 'edit'
+    end
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
@@ -23,5 +39,19 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
