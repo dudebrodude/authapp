74b1de5c09540a656a82426efb18a763d6b07b8f
Daniel Babicky <dudebrodudelol@gmail.com>
2014-08-13 17:37:50 -0700

    prepare for fa

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -4,7 +4,7 @@ class ClubsController < ApplicationController
   # GET /clubs
   # GET /clubs.json
   def index
-    @clubs = Club.all
+      @clubs = Club.paginate(page: params[:page])
   end
 
   # GET /clubs/1
