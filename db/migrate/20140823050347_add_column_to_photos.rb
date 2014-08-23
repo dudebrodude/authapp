a6ac80eddf25187bac355d7074889019d518e2f7
Daniel Babicky <dudebrodudelol@gmail.com>
2014-08-22 23:22:05 -0700

    another db fix

--- db/migrate/20140823050347_add_column_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToPhotos < ActiveRecord::Migration
-  def change
-    add_column :photos, :image_name, :string
-  end
-end
--- db/migrate/20140823050559_add_column2_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddColumn2ToPhotos < ActiveRecord::Migration
-  def change
-    add_column :photos, :image_uid, :string
-  end
-end
