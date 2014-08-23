e5c666579b0f3fd69195a7679431042c36ae7fd9
Daniel Babicky <dudebrodudelol@gmail.com>
2014-08-22 23:19:24 -0700

    fixing db for heroku

--- db/migrate/20140823050030_remove_asset_uid_from_photos.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAssetUidFromPhotos < ActiveRecord::Migration
-  def change
-    remove_column :photos, :asset_name, :string
-  end
-end
--- db/migrate/20140823050206_remove_asset_name_from_photos.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAssetNameFromPhotos < ActiveRecord::Migration
-  def change
-    remove_column :photos, :asset_uid, :string
-  end
-end
